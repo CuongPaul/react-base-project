3cd127faf59580527afd943fa6fbc6613acde3a8
Ta Cuong <cuongvt716@gmail.com>
2025-04-23 15:39:35 +0700

    Scaling app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { useReducer } from "react";
+import { useRef, useEffect, useReducer } from "react";
 import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
 
+import "./App.scss";
 import { ACCESS_TOKEN } from "./types";
 import { AppContext } from "./contexts";
 import { PrivateRoute } from "./components";
@@ -10,26 +11,41 @@ import { AccountReducer, initialAccountState } from "./contexts/account";
 const App = () => {
   const token = localStorage.getItem(ACCESS_TOKEN);
 
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
   const [accountState, accountDispatch] = useReducer(
     AccountReducer,
     initialAccountState
   );
 
+  useEffect(() => {
+    if (wrapperRef.current) {
+      const widthByDesign = 960;
+      const { innerWidth } = window;
+
+      const proportion = Math.floor((innerWidth * 10) / widthByDesign) / 10;
+
+      wrapperRef.current.style.setProperty("--proportion", `${proportion}`);
+    }
+  }, []);
+
   return (
-    <AppContext.Provider value={{ accountState, accountDispatch }}>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<PrivateRoute />}>
-            <Route path="/home" element={<Home />} />
-          </Route>
-          <Route
-            path="/signin"
-            element={token ? <Navigate to="/home" /> : <Signin />}
-          />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </BrowserRouter>
-    </AppContext.Provider>
+    <div ref={wrapperRef} className="app-wrapper">
+      <AppContext.Provider value={{ accountState, accountDispatch }}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<PrivateRoute />}>
+              <Route path="/home" element={<Home />} />
+            </Route>
+            <Route
+              path="/signin"
+              element={token ? <Navigate to="/home" /> : <Signin />}
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </AppContext.Provider>
+    </div>
   );
 };
 
