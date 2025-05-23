4691c37fa9cdfea674fb378b1a69063de006cc6d
CuongPaul <100858446+CuongPaul@users.noreply.github.com>
2025-05-23 18:23:12 +0700

    Character action

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,21 +5,96 @@ import "./index.scss";
 const Home = () => {
   const characterRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
+  const updateCharacter = ({
+    speed,
+    imageUrl,
+    imageWidth,
+    imageHeight,
+    numberOfFrame,
+  }: {
+    speed: number;
+    imageUrl: string;
+    imageWidth: number;
+    imageHeight: number;
+    numberOfFrame: number;
+  }) => {
     if (characterRef.current) {
       characterRef.current.style.setProperty(
-        "--image-url",
-        "url('/images/1.png')"
+        "--image-height",
+        `${imageHeight}px`
       );
-      characterRef.current.style.setProperty("--number-of-frame", "13");
-      characterRef.current.style.setProperty("--image-width", "6500px");
-      characterRef.current.style.setProperty("--image-height", "300px");
+      characterRef.current.style.setProperty(
+        "--image-width",
+        `${imageWidth}px`
+      );
+      characterRef.current.style.setProperty(
+        "--number-of-frame",
+        `${numberOfFrame}`
+      );
+      characterRef.current.style.setProperty(
+        "--image-url",
+        `url('/images/${imageUrl}')`
+      );
+      characterRef.current.style.setProperty("--speed", `${speed}s`);
     }
+  };
+
+  useEffect(() => {
+    updateCharacter({
+      speed: 2,
+      imageHeight: 80,
+      imageWidth: 480,
+      numberOfFrame: 8,
+      imageUrl: "samara-1.png",
+    });
+  }, []);
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      switch (event.key) {
+        case "j":
+          updateCharacter({
+            speed: 1.5,
+            imageWidth: 420,
+            imageHeight: 100,
+            numberOfFrame: 7,
+            imageUrl: "samara-2.png",
+          });
+
+          break;
+
+        case "d":
+          updateCharacter({
+            speed: 1,
+            imageHeight: 80,
+            imageWidth: 360,
+            numberOfFrame: 6,
+            imageUrl: "samara-3.png",
+          });
+
+          break;
+
+        default:
+          updateCharacter({
+            speed: 2,
+            imageHeight: 80,
+            imageWidth: 480,
+            numberOfFrame: 8,
+            imageUrl: "samara-1.png",
+          });
+
+          break;
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => document.removeEventListener("keydown", handleKeyDown);
   }, []);
 
   return (
     <div className="home-wrapper">
-      <div ref={characterRef} className="character single-character" />
+      <div ref={characterRef} className={`character animated`} />
     </div>
   );
 };
