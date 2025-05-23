79bd13e0ec9293daf4e27d2bdba6c6c154232eaf
CuongPaul <100858446+CuongPaul@users.noreply.github.com>
2025-05-23 15:08:32 +0700

    Gogeta kamehameha

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,25 @@
+import { useRef, useEffect } from "react";
+
 import "./index.scss";
 
 const Home = () => {
+  const characterRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (characterRef.current) {
+      characterRef.current.style.setProperty(
+        "--image-url",
+        "url('/images/1.png')"
+      );
+      characterRef.current.style.setProperty("--number-of-frame", "13");
+      characterRef.current.style.setProperty("--image-width", "6500px");
+      characterRef.current.style.setProperty("--image-height", "300px");
+    }
+  }, []);
+
   return (
     <div className="home-wrapper">
-      <img alt="" src="/images/prize.png" className="home-wrapper__img" />
+      <div ref={characterRef} className="character single-character" />
     </div>
   );
 };
